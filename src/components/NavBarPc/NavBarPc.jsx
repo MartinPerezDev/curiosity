3841eef718e5bc5d4fa2ddc7a091c064928fdf5d
EfimeroM <martinperez9713@gmail.com>
2023-01-08 22:45:02 -0300

    fix: navBarPc

--- src/components/NavBarPc/NavBarPc.jsx
+++ src/components/NavBarPc/NavBarPc.jsx
@@ -4,36 +4,60 @@ import "./NavBarPc.scss";
 import { ThemeContext } from "../../context/ThemeContext";
 import { CiLight, CiDark } from "react-icons/ci";
 import { VscHome, VscSymbolMethod, VscTerminalDebian, VscKey } from "react-icons/vsc";
+import { AiOutlineMail } from "react-icons/ai";
 import { Link } from "react-router-dom";
 
 export const NavBarPc = () => {
   const { theme, setTheme, color } = useContext(ThemeContext);
+  const categoriesMenu = [
+    {
+      name: "Inicio",
+      route: "/",
+      icon: <VscHome />
+    },
+    {
+      name: "Curiosidades",
+      route: "/curiosidades",
+      icon: <VscSymbolMethod />
+    },
+    {
+      name: "Sobre Nosotros",
+      route: "/sobre-nosotros",
+      icon: <VscTerminalDebian />
+    },
+    {
+      name: "Contacto",
+      route: "/contacto",
+      icon: <AiOutlineMail />
+    }
+  ]
 
   return (
     <div id="nav-bar-pc">
       <div className="banner">
         <FcDebian className="logo" />
       </div>
-      <div
-        className="menu"
-        style={{ background: `${color.background}`}}
-      >
+      <div className="menu" style={{ background: `${color.background}`}}>
         <div className="brand">
           <FcDebian className="logo" />
           <p>Curiosity</p>
         </div>
         <ul>
-          <Link to={"/"} className="link" style={{ color: `${color.text}`}}><VscHome /><p>Inicio</p></Link>
-          <Link to={"/curiosidades"} className="link" style={{ color: `${color.text}`}}><VscSymbolMethod /><p>Curiosidades</p></Link>
-          <Link to={"/sobre-nosotros"} className="link" style={{ color: `${color.text}`}}><VscTerminalDebian /><p>Sobre Nosotros</p></Link>
-          <Link to={"/"} className="link" style={{ color: `${color.text}`}}><VscKey /><p>Iniciar Sesion</p></Link>
+          {
+            categoriesMenu.map(categorie=>{
+              return <Link to={categorie.route} className="link" style={{ color: `${color.text}`}}>
+                {categorie.icon}
+                <p>{categorie.name}</p>
+              </Link>
+            })
+          }
         </ul>
         <div className="theme-button" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
             {
-                theme === "light" ? 
-                <CiDark className="button"/>
-                :
-                <CiLight className="button"/>
+              theme === "light" ? 
+              <CiDark className="button"/>
+              :
+              <CiLight className="button"/>
             }   
         </div>
       </div>
